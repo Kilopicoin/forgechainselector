a1a23b571d0fff607584a9d073406869a3a157c9
Cem Tosun <cemtosun@mail.com>
2024-12-18 07:28:39 +0300

    18 Dec 2024

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css'; // Import a CSS file for styling
 import solanaLogo from './assets/solanaLogo.png';
 import harmonyLogo from './assets/harmonyLogo.png';
+import binanceLogo from './assets/binanceLogo.png';
 
 function App() {
   return (
@@ -36,6 +37,23 @@ function App() {
             Create Token on Harmony
           </a>
         </div>
+
+
+        <div className="chain-card binance">
+          <img src={binanceLogo} alt="Binance Logo" className="chain-logo" />
+          <h2>Binance Smart</h2>
+          <a 
+            href="https://kilopi.net/forge/binancesmart/" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="create-button"
+          >
+            Create Token on BSC
+          </a>
+        </div>
+
+
+
       </div>
     </div>
   );
